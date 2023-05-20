3a714516ced9d8bca25d4258b89c3a0e192d0f39
sunnysamuelms <sunnysamuel2112@gmail.com>
2023-05-21 00:00:46 +0530

    functioning for few pictures

--- app.py
+++ app.py
@@ -1,58 +1,51 @@
-from flask import Flask, Response
+from flask import Flask, render_template, request
 import cv2
 from deepface import DeepFace
+import numpy as np
 
 app = Flask(__name__)
 
+# Load the cascade classifier
 face_cascade = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
 
+# Define the home page
+@app.route('/', methods=['GET', 'POST'])
+def home():
+    if request.method == 'POST':
+        # Get the image file from the request
+        file = request.files['image']
 
-def detect_faces():
-    cap = cv2.VideoCapture(0)
-    while True:
-        # Read a frame from the video capture
-        ret, frame = cap.read()
+        # Read the image file as an array
+        image = cv2.imdecode(np.fromstring(
+            file.read(), np.uint8), cv2.IMREAD_COLOR)
 
-        # Convert the frame to grayscale
-        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-
-        # Detect faces in the grayscale frame
+        # Detect faces in the image
+        gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
         faces = face_cascade.detectMultiScale(
             gray, scaleFactor=1.3, minNeighbors=5)
 
-        # Draw a rectangle around each face and detect emotions
+        # Initialize the results table
+        results = []
+
+        # Analyze each face in the image
         for (x, y, w, h) in faces:
-            cv2.rectangle(frame, (x, y), (x+w, y+h), (255, 0, 0), 2)
-            face_cropped = frame[y:y+h, x:x+w]
+            # Crop the face region
+            face_cropped = image[y:y+h, x:x+w]
+
+            # Analyze the cropped face using DeepFace
             predictions = DeepFace.analyze(
                 face_cropped, actions=['emotion'], enforce_detection=False)
 
-            # Adjust the position and font size of the text
-            cv2.putText(frame, predictions[0]['dominant_emotion'], (
-                x+w+10, y), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0), 2, cv2.LINE_AA)
+            # Add the dominant emotion to the results table
+            dominant_emotion = predictions[0]['dominant_emotion'].upper()
+            results.append(dominant_emotion)
 
-        # Convert the processed frame to a byte array
-        _, jpeg = cv2.imencode('.jpg', frame)
-        frame_bytes = jpeg.tobytes()
+        # Render the results page
+        return render_template('index.html', results=results)
 
-        # Yield the frame as a byte array to the client
-        yield (b'--frame\r\n'
-               b'Content-Type: image/jpeg\r\n\r\n' + frame_bytes + b'\r\n')
-
-        # Break the loop if the 'q' key is pressed
-        if cv2.waitKey(1) & 0xFF == ord("q"):
-            break
-
-    # Release the video capture and destroy the window
-    cap.release()
-    cv2.destroyAllWindows()
-
-
-@app.route('/')
-def video_feed():
-    return Response(detect_faces(),
-                    mimetype='multipart/x-mixed-replace; boundary=frame')
-
+    # Render the home page
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
+
